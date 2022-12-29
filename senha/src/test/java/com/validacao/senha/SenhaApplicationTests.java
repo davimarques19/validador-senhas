d789cf92751da7c145cfe4428a2d933d5baf672f
Davi Marques <daviestudos19@gmail.com>
2022-12-29 11:55:30 -0300

    task-0001 - Adicionando estrututa inicial do projeto

---
+++ senha/src/test/java/com/validacao/senha/SenhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.validacao.senha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
