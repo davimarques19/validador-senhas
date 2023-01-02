7afb001e874ae1ff609a786611b18db5baae9103
Davi Marques <daviestudos19@gmail.com>
2023-01-02 16:03:51 -0300

    task-0012 - Refatorando códido e classes

--- senha/src/main/java/com/validacao/senha/service/PasswordService.java
+++ senha/src/main/java/com/validacao/senha/service/PasswordService.java
@@ -2,23 +2,29 @@ package com.validacao.senha.service;
 
 import com.validacao.senha.controller.representation.PasswordRequest;
 import com.validacao.senha.domain.Password;
+import com.validacao.senha.exception.PasswordBadRequestException;
 import com.validacao.senha.repository.PasswordRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class PasswordService {
 
-    @Autowired
-    PasswordRepository repository;
+    private final PasswordRepository repository;
+    private final ValidatorPasswordService validatorPasswordService;
 
-    @Autowired
-    ValidatorPasswordService validatorPasswordService;
+    public PasswordService(PasswordRepository repository, ValidatorPasswordService validatorPasswordService) {
+        this.repository = repository;
+        this.validatorPasswordService = validatorPasswordService;
+    }
 
-    public Password getPassword(PasswordRequest password) {
-        log.info("Iniciando consulta UserService.getPassword");
+    public Password validatePassword(PasswordRequest password) {
+        log.info("Iniciando consulta UserService.validatePassword");
+
+        if (password.getInput() == null) {
+            throw new PasswordBadRequestException("Erro");
+        }
 
         return repository.save(validatorPasswordService.validate(password));
 
